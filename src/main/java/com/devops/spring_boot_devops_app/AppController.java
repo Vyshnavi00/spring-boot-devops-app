4233d7f41272fd1e47ed710b5229d9df6e81fa37
Vyshnavi00 <drvyshnavi00@gmail.com>
2026-05-12 15:54:23 +0530

    Updated AppController.java file

--- src/main/java/com/devops/spring_boot_devops_app/AppController.java
+++ src/main/java/com/devops/spring_boot_devops_app/AppController.java
@@ -9,7 +9,7 @@ public class AppController {
 
     @GetMapping("/")
     public String home() {
-        return "Spring Boot DevOps App is Running!";
+        return "Spring Boot DevOps App - v2 Deployed via CI/CD!";
     }
 
     @GetMapping("/health")
